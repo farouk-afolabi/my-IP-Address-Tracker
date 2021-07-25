931239247375bbe4ca2106ed8d0039cf7187e3ad
farouk afolabi <afolabifarouk99@gmail.com>
2021-07-25 10:41:49 +0300

    The process

--- portfolio_website-STARTER/src/components/Projects/Projects.js
+++ portfolio_website-STARTER/src/components/Projects/Projects.js
@@ -7,9 +7,34 @@ import { projects } from '../../constants/constants';
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
-    <SectionTitle> Projects </SectionTitle>
-     
+    <SectionTitle main> Projects </SectionTitle>
+     <GridContainer>
+       {projects.map(( { id, image, title,description, tags ,source, visit }) => (
+         <BlogCard key= {id}>
+           <Img src={image} />
+           <TitleContent>
+             <HeaderThree title>{title}</HeaderThree>
+             <Hr/>
+           </TitleContent>
+           <CardInfo>{description}</CardInfo>
+           <div>
+             <TitleContent>
+               Stack
+             </TitleContent>
+             <TagList>
+               { tags.map((tag, i) => (
+                 <Tag key={i}>{tag}</Tag>
+               ))}  
+             </TagList>
+           </div>
+           <UtilityList>
+             <ExternalLinks href ={visit}>Code </ExternalLinks>
+             <ExternalLinks href ={source}>Source </ExternalLinks>
+           </UtilityList>
+         </BlogCard>
+       ) )}
+     </GridContainer>
   </Section>
 );
 
-export default Projects;
+export default Projects;  
--- portfolio_website-STARTER/src/components/Technologies/Technologies.js
+++ portfolio_website-STARTER/src/components/Technologies/Technologies.js
@@ -4,9 +4,48 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+<Section id="tech">
+  <SectionDivider/>
+  <SectionTitle>Technologies</SectionTitle>
+  <SectionText>
+    I have worked with a range of technologies in the web development world.From Front-end To Design
+  </SectionText>
+<List>
+  <ListItem>
+    <DiFirebase size="3rem"/>
+    <ListContainer>
+      <ListTitle> Front-End </ListTitle>
+       <ListParagraph>
+         Experience with <br/>
+         React.js
+         </ListParagraph> 
+      
+    </ListContainer>
+  </ListItem>
+  <ListItem>
+    <DiFirebase size="3rem"/>
+    <ListContainer>
+      <ListTitle> Front-End </ListTitle>
+       <ListParagraph>
+         Experience with <br/>
+         React.js
+         </ListParagraph> 
+      
+    </ListContainer>
+  </ListItem>
+  <ListItem>
+    <DiFirebase size="3rem"/>
+    <ListContainer>
+      <ListTitle> Front-End </ListTitle>
+       <ListParagraph>
+         Experience with <br/>
+         React.js
+         </ListParagraph> 
+      
+    </ListContainer>
+  </ListItem>
+</List>
+</Section>
 );
 
 export default Technologies;
